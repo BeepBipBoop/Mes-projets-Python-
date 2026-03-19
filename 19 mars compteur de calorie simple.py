290b13fc7146620357376810506bc1a97d951def
BeepBipBoop <eliasaliaselias@gmail.com>
2026-03-19 16:44:45 +0100

    Add files via upload

---
+++ 19 mars compteur de calorie simple.py
@@ -0,0 +1,15 @@
+# Entrez votre métabolisme basale ici ! 
+
+def Kcal_tracker():
+  print ("Entrez votre métabolisme")
+  metabolism = input()
+  kcal_daily = float(metabolism) - 100 
+  print (f"Votre vrai métabolisme est {kcal_daily}")
+
+
+
+
+
+Kcal_tracker()
+
+
