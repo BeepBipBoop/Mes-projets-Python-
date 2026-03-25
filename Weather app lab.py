064c4c6d1d9b044f4c2adb944fe2ecd919c1d7fe
BeepBipBoop <eliasaliaselias@gmail.com>
2026-03-25 16:18:49 +0100

    Add files via upload

---
+++ Weather app lab.py
@@ -0,0 +1,27 @@
+distance_mi = float(1)
+is_raining = False
+has_bike = False
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print("False")
+elif distance_mi <= 1:
+    if not is_raining:
+        print("True")
+    else:
+        print("False")
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print("True")
+    else:
+        print("False")
+else:  # > 6 miles
+    if has_car or has_ride_share_app:
+        print("True")
+    else:
+        print("False")
+
+
+
+#Freecode.camp
