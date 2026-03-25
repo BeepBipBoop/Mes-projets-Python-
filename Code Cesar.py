62608d8829827a3df9a74aaa6182daf2fbb10094
BeepBipBoop <eliasaliaselias@gmail.com>
2026-03-25 15:18:37 +0100

    Implement Caesar cipher functions
    Added functions for Caesar cipher encryption and decryption.

--- Code Cesar.py
+++ Code Cesar.py
@@ -1,8 +1,30 @@
 #Un code césar c'est une facon de communication qui consiste a décaller l'alphabet du texte brute avec un ordre x.
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = (alphabet[shift:])
+def caesar(text, shift, encrypt=True):
 
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
 
-print(shifted_alphabet)
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'      
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+encrypted_text = encrypt('Pbhentr vf sbhaq va hayvxryl cynprf.',13)
+print(encrypted_text)
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
